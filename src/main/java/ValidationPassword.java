553da5a82c0f11b78c6d4dce102cf6f87525aefc
Pourya <pourya.nejadensan@gmail.com>
2024-07-02 13:03:31 +0200

    define signatures

---
+++ src/main/java/ValidationPassword.java
@@ -0,0 +1,14 @@
+public class ValidationPassword {
+    public static boolean isPasswordEightChar(String password) {
+        return true;
+    }
+    public static boolean hasPasswordDigit(String password) {
+        return true;
+    }
+    public static boolean hasPasswordUpperAndLowerLetter(String password) {
+        return true;
+    }
+    public static boolean isPasswordCommon(String password) {
+        return true;
+    }
+}
